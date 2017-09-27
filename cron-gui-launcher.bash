4eb043ece71c3cbdc93ac03a2a132533a1c9f61b
pa4080 <spas.z.spasov@gmail.com>
2017-09-28 01:55:02 +0300

    Just update

--- cron-gui-launcher.bash
+++ cron-gui-launcher.bash
@@ -3,7 +3,7 @@
 # Create log file. Use "$2" to leave a description within the name.
 if [ -z "${2+x}" ]; then DESCRIPTION=""; else DESCRIPTION="-$2"; fi
 LOG="/tmp/$USER-cron-gui-launcher$DESCRIPTION.log"
-printf "\n$(date +%Y-%m-%d_%H:%M:%S)\n\nDetected environment variables:\n" > $LOG
+printf "\n$(date +%Y-%m-%d_%H:%M:%S)\n\n\nDetected environment variables:\n\n" > $LOG
 
 # Get the value of the $DISPLAY variable for the current user. Unset it just in case this is a `ssh -X` connection
 unset DISPLAY
@@ -15,48 +15,48 @@ done; printf "DISPLAY=$DISPLAY\n" >> $LOG
 # --------------------
 
 # Get the most frequent value from an array any array - https://stackoverflow.com/a/43440769/6543935
-get_frequent(){ 
+get_frequent(){
         awk 'BEGIN{ FS=" " } { for(i=1;i<=NF;i++) print $i }' | \
         awk '{ n=++hsh[$1]; if(n>max_occ){ max_occ=n; what=$1 } else if(n==max_occ){ if(what>$1) what=$1 } } END{ print what }'
 }
 
 # Get certain envvar value ("$1") from any "/proc/$ProcessNumber/environ" file ("$2")
-get_environ(){ envvar=$(sed -zne "s/^$1=//p" "/proc/$2/environ" 2>/dev/null); printf "$envvar"; }
+get_environ(){
+	EnVar=$(sed -zne "s/^$1=//p" "/proc/$2/environ" 2>/dev/null); printf "$EnVar";
+}
 
 #
 export_environ(){
-        printf "\nExported environment:\n\nSource file: /proc/$1/environ\n\n" >> $LOG
-        IFS_BAK="$IFS"; IFS='^@'; 
-        #for envvar in $(cat -e "/proc/$1/environ"); do printf "$envvar\n" | sed '/^\s*$/d' >> $LOG; export $envvar; done
-        for env in $(cat -e "/proc/$1/environ"); do export $(printf "${env}\n" | sed '/^\s*$/d'); done
-        #for envvar in $(cat -e "/proc/$(pgrep gnome-session -n -U $UID)/environ"); do export $envvar >> $LOG; done
-        #ENVIRON=$(cat -e "/proc/$1/environ" | sed '/^\s*$/d')
-        #for envvar in ; do printf "$envvar\n" | sed '/^\s*$/d' >> $LOG; export $envvar; done
-        
-        #echo $ENVIRON
-        for i in ${ENVIRON[@]}; do echo -e "$i\n"; done
-        IFS="$IFS_BAK"
+        printf "\n\nExported environment (source file /proc/$1/environ):\n\n" >> $LOG
+	for EnVar in $(cat -e "/proc/$1/environ" | sed 's/\^@/\n/g'); do echo "export $EnVar" >> $LOG; export "$EnVar"; done
+}
+
+execute_input_commands(){
+	printf "$1" | awk 'BEGIN{FS=" && "; print "\n\nInput command list:"} {for(i=1;i<=NF;i++) system("echo \"Command: " $i "\" && nohup " $i "& ")}' >> $LOG
 }
 
 # Get the values of $XDG_CURRENT_DESKTOP from each "/proc/$ProcessNumber/environ" file - create an array.
 # Get the most frequent name of any desctop environment - within the created array # This is a way to find the current DE when it is changed a little bit ago
 for PN in $(pgrep -U "$UID"); do XDG_CURRENT_DESKTOP+=$(get_environ "XDG_CURRENT_DESKTOP" "$PN"; echo " "); done
-XDG_CURRENT_DESKTOP=$(echo -e ${XDG_CURRENT_DESKTOP[@]} | get_frequent) 
-declare -l DE="${XDG_CURRENT_DESKTOP/:*/}" && printf "XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP\nDE=$DE\n" >> $LOG
+XDG_CURRENT_DESKTOP=$(echo -e ${XDG_CURRENT_DESKTOP[@]} | get_frequent)
+declare -l DE && export DE="${XDG_CURRENT_DESKTOP/:*/}" && printf "XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP\nDE=$DE\n" >> $LOG
 
 # ---------------------------
 
 # Export the Desktop Environment:
 if   [ "$DE" = "unity" ];               then export_environ "$(pgrep gnome-session -n -U $UID)"
-#elif [ "$DE" = "gnome" ];               then export_environ "$(pgrep gnome-session -n)"
-#elif [ "$DE" = "gnome-classic" ];       then export_environ "$(pgrep gnome-session -n)"
-#elif [ "$DE" = "kde" ];                 then export_environ "$(pgrep startkde -n)"
-#elif [ "$DE" = "mate" ];                then export_environ "$(pgrep mate-session  -n)"
-#elif [ "$DE" = "lxde" ];                then export_environ "$(pgrep lxsession -n)"
-#elif [ "$DE" = "xfce" ];                then export_environ "$(pgrep xfce4-session -n)"
-#elif [ "$DE" = "xfce4" ];               then export_environ "$(pgrep xfce4-session -n)"
+elif [ "$DE" = "gnome" ];               then export_environ "$(pgrep gnome-session -n)"
+elif [ "$DE" = "gnome-classic" ];       then export_environ "$(pgrep gnome-session -n)"
+elif [ "$DE" = "kde" ];                 then export_environ "$(pgrep startkde -n)"
+elif [ "$DE" = "mate" ];                then export_environ "$(pgrep mate-session  -n)"
+elif [ "$DE" = "lxde" ];                then export_environ "$(pgrep lxsession -n)"
+elif [ "$DE" = "xfce" ];                then export_environ "$(pgrep xfce4-session -n)"
+elif [ "$DE" = "xfce4" ];               then export_environ "$(pgrep xfce4-session -n)"
 else printf "Your current Desktop Environment is not supported!\n Please contribute to https://github.com/pa4080/cron-gui-launcher\n" >> $LOG
 fi
 
+execute_input_commands "$1"
+
 # Debug --------
 cat $LOG
+
