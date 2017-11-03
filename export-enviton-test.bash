c335a33b002fe4f5067790b686a4ffbc17f0987d
pa4080 <spas.z.spasov@gmail.com>
2017-11-03 05:40:03 +0200

    Create export-enviton-test.bash

---
+++ export-enviton-test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DE_SESSION="${1}"
+
+# Check whether the user is logged in Mate
+while [ -z "$(pgrep "$DE_SESSION" -n -U $UID)" ]; do
+        sleep 3 && count=$((count+1)) && echo "$count"
+done
+
+# Get the content of the Current-Desktop-Session Environment File as an array:
+EnvVarList=`cat -e "/proc/$(pgrep "$DE_SESSION" -n -U $UID)/environ" | sed 's/\^@/\n/g'`
+
+# Export the Current-Desktop-Session Environment Variables:
+for EnvVar in $EnvVarList; do
+        echo "$EnvVar"
+        export "$EnvVar"
+done
