4ce713364cc9cef0d83cf621da743578b43ae980
pa4080 <spas.z.spasov@gmail.com>
2021-07-13 18:41:07 +0300

    xRDP Ubuntu 20.04

--- cron-gui-launcher.bash
+++ cron-gui-launcher.bash
@@ -8,15 +8,34 @@ printf '\n%s\n\n\nDetected environment variables:\n\n' "$(date +%Y-%m-%d_%H:%M:%
 # [1.] Get the value of the $DISPLAY variable for the current user. Unset it just in case this is a `ssh -X` connection
 unset DISPLAY; timeout=0
 while [[ -z "$DISPLAY" ]]; do
-        DISPLAY=$(w "$USER" | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}' 2>/dev/null)
-        if [[ "$DISPLAY" == "" ]]; then sleep 60; else export DISPLAY=$DISPLAY; fi
+        DISPLAY=$(w -h "$USER" | awk 'NF > 0 && $2 ~ /tty[0-9]+|^:/ {print $3; exit}' 2>/dev/null)
+
+        if [[ "$DISPLAY" == "" ]]
+        then
+                echo $DISPLAY
+                XRDP_DISPLAY=$(ps -ef | grep -oP '/usr/lib/xorg/Xorg :[0-9]+ -auth .Xauthority.*log$' | grep -oP ':[0-9]+')
+                if [[ "$XRDP_DISPLAY" != "" ]]
+                then
+                        export DISPLAY=${XRDP_DISPLAY}.0
+                else
+                        sleep 5
+                fi
+        else
+                export DISPLAY=$DISPLAY
+        fi
+
         ((timeout++))
-        if [[ ! -z "${3+x}" ]] && [[ "$timeout" -eq "$3" ]]; then printf "Timeout: %s\n" "$timeout" >> "$LOG"; exit 1; fi
+
+        if [[ ! -z "${3+x}" ]] && [[ "$timeout" -eq "$3" ]]
+        then
+                printf "Timeout: %s\n" "$timeout" >> "$LOG"
+                exit 1
+        fi
 done; printf 'DISPLAY=%s\n' "$DISPLAY" >> "$LOG"
 
 # [->2.] Get certain envvar value ("$1") from any "/proc/$ProcessNumber/environ" file ("$2")
 get_environ(){
-	EnvVar=$(sed -zne "s/^$1=//p" "/proc/$2/environ" 2>/dev/null); printf "%s" "$EnvVar";
+        EnvVar=$(sed -zne "s/^$1=//p" "/proc/$2/environ" 2>/dev/null); printf "%s" "$EnvVar";
 }
 
 # [->3.] Get the most frequent value from an array - https://stackoverflow.com/a/43440769/6543935
@@ -29,7 +48,7 @@ get_frequent(){
 export_environ(){
         printf '\n\nExported environment (source file /proc/%s/environ):\n\n' "$1" >> "$LOG"
         EnvVarList=$(cat -e "/proc/$1/environ" | sed 's/\^@/\n/g')
-	for EnvVar in $EnvVarList; do echo "export $EnvVar" >> "$LOG"; export "$EnvVar"; done
+        for EnvVar in $EnvVarList; do echo "export $EnvVar" >> "$LOG"; export "$EnvVar"; done
 }
 
 # [->6.] Fragmentation of the list of the input commands (input variable "$1"), use ` && ` as separator, then execute each one
@@ -43,7 +62,7 @@ for PN in $(pgrep -U "$UID"); do XDG_CURRENT_DESKTOP+=$(get_environ "XDG_CURRENT
 # [3.] Get the name of the current Desktop Environment
 XDG_CURRENT_DESKTOP=$(echo -e "${XDG_CURRENT_DESKTOP[@]}" | get_frequent)
 
-# [4.] Set the triger $DE - declare it in lowercase, then cut `:something` and export 
+# [4.] Set the triger $DE - declare it in lowercase, then cut `:something` and export
 declare -l DE && export DE="${XDG_CURRENT_DESKTOP/:*/}" && printf 'XDG_CURRENT_DESKTOP=%s\nDE=%s\n' "$XDG_CURRENT_DESKTOP" "$DE" >> "$LOG"
 
 # [5.] Export the Current-Desktop-Session Environment Variables:
@@ -61,9 +80,9 @@ fi
 
 # [6.] Execute the list of the input commands
 if [[ -z "${1+x}" ]]; then
-	printf '\n\nThere is not any input command!\n'
+        printf '\n\nThere is not any input command!\n'
 else
-	execute_input_commands "$1"
+        execute_input_commands "$1"
 fi
 
 # Debug cat "$LOG"
